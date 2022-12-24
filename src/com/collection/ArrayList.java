6d7caccef5c3d082e7741bd1946b9c0643aa40d7
ReshmaMohiteGitHub <reshmamohite06@gmail.com>
2022-12-24 23:53:48 +0530

    second commit: linkedlist code

---
+++ src/com/collection/ArrayList.java
@@ -0,0 +1,15 @@
+package com.collection;
+
+import java.util.LinkedList;
+
+public class ArrayList {
+	public static void main(String[] args) {
+		LinkedList<String> list = new LinkedList<String>();
+		list.add("neha");
+		list.add("riya");
+		System.out.println(list);
+
+	}
+
+
+}
